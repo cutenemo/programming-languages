bd3ab87c8f7e6823c26f77f096cb470314d7d396
Fatima Broom <fbroom@apple.com>
2017-02-05 19:48:11 -0800

    read-line for the future

---
+++ c++/read-line.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  uhunt
+//
+//  read line
+//  2017/02/06
+//
+
+#include <iostream>
+
+int main() {
+    //freopen("sample.in", "r", stdin);
+
+    int K = 0, E = 0, n = 0;
+    while(scanf("%d%d\n", &K, &E) == 2) { // or != EOF both work
+        n++;
+    }
+
+    std::string line;
+    while (getline(std::cin, line)) {
+
+    }
+
+    char sentence[50];
+    while (fgets(sentence, 50, stdin)) {
+
+    }
+
+    return 0;
+}
